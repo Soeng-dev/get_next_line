abca6d2d757ef4c81ef628c30a9cfb963c8c1282
yong-dev <tjddyd1565@naver.com>
2021-01-06 01:14:13 +0900

    after copy to tester folder, deleted unnecessary structure in header file

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soekim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/22 19:32:19 by soekim            #+#    #+#             */
+/*   Updated: 2020/11/29 10:28:28 by soekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+# define ERROR		-1
+# define END		0
+# define SUCCESS	1
+
+typedef struct s_file	t_file;
+typedef struct s_cat	t_cat;
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int		strdel_len(char *s, char delimiter);
+int		strcat_del(char **line, char *to_catenate, char delimiter);
+int		min(int a, int b);
+int		get_next_line(int fd, char **line);
+#endif
