ca4d44b042fdd03b7b8287f816dbdff9fbcc12d7
yong-dev <tjddyd1565@naver.com>
2021-01-07 19:08:25 +0900

    testers redownloaded, restored gnl codes

---
+++ subject_file/gnlkiller/get_next_line.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soekim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/22 19:32:19 by soekim            #+#    #+#             */
+/*   Updated: 2020/11/29 17:47:48 by soekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		get_next_line(int fd, char **line)
+{
+	static char	backup[BUFFER_SIZE + 1];
+	static char	*next;
+	int		result;
+	int		unread;	
+
+	unread = 1;
+	while (1)
+	{
+		if (!next || next >= backup + BUFFER_SIZE)
+		{
+			result = read(fd, backup, BUFFER_SIZE);
+			backup[result] = '\0';
+			//printf("read len : %d\ncontents\n%s////\n---------------------\n",result, backup);
+			if (result == ERROR || result == END)
+				return (result);
+			unread = result;
+			next = backup;
+		}
+		else if (unread == 0)
+			return (END);
+			//printf("unread : %d\n", unread);
+		result = strcat_del(line, next, '\n');
+		if (result == ERROR)
+			return (ERROR);
+		unread -= (result + 1);
+		next += (result + 1);
+		//printf("next : %p||	%s\n",next,next);
+		//printf("oneloopdone=====================\n");
+	}
+	return (SUCCESS);
+}
+int main(void)
+{
+	char *line = 0;
+	int ret;
+	int fd;
+
+	fd = open("testfile2", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}
---
+++ subject_file/ownTest/get_next_line_test.c
@@ -0,0 +1,31 @@
+#include "../get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+void 	putstr(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while(*s)
+	{	write(fd, s, 1);
+		s++;
+	}
+}
+
+void	main(void)
+{
+	int	fd = open("./for_test.txt", O_RDWR);
+	char 	*read = (char *)malloc(1);
+	*read = '\0';
+	int i = SUCCESS;
+	while(i == SUCCESS)
+	{
+		i = get_next_line(fd, &read);
+	}
+	printf("\n\nresult\n\n");
+	printf("%s\n",read);
+
+}
