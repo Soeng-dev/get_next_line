a99deddd283676d90ce45616807dae7262cfc83e
Soengyong Kim <soekim@c1r7s4.42seoul.kr>
2021-01-26 20:31:06 +0900

    42testers gnl done except bonus, still makefile test of GNL lover need to be solved

--- subject_file/get_next_line.c
+++ subject_file/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: soekim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 19:32:19 by soekim            #+#    #+#             */
-/*   Updated: 2021/01/26 18:21:41 by soekim           ###   ########.fr       */
+/*   Updated: 2021/01/26 20:26:25 by soekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 
 int		get_next_line(int fd, char **line)
 {
-	static char *backup[OPEN_MAX];
-	char		buffer[BUFFER_SIZE + 1];
+	static char *backup;
+	static char	buffer[BUFFER_SIZE + 1];
 	char		*temp;
 	int			is_oneline;
 	int			result;
 
 	if (fd < 0 || !line)
 		return (ERROR);
+	result = 1;
 	is_oneline = 0;
 	temp = NULL;
 	while (!is_oneline)
 	{
-		if (!backup[fd])
+		if (!backup)
 		{
 			if ((result = read(fd, buffer, BUFFER_SIZE)) == ERROR)
 				return (ERROR);
 			buffer[result] = '\0';
-			backup[fd] = buffer;
+			backup = buffer;
 		}
-		backup[fd] += strcat_del(&temp, backup[fd], '\n');
-		if (*(backup[fd]) == '\n' || result == END)
-		{
-			++backup[fd];
+		backup += strcat_del(&temp, backup, '\n');
+		if (*(backup) == '\n' || result == END)
 			is_oneline = 1;
-		}
-		if (*(backup[fd]) == '\0')
-			backup[fd] = NULL;
+		if (*(backup) == '\n')
+			++backup;
+		if (*(backup) == '\0')
+			backup = NULL;
 	}
 	*line = temp;
 	if (result == END)
 	{
-		backup[fd] = NULL;
+		backup = NULL;
 		return (END);
 	}
 	return (SUCCESS);
