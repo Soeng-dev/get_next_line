18b080190871ed372740879026de501adebfd771
Soengyong Kim <soekim@c1r7s4.42seoul.kr>
2021-01-27 17:58:26 +0900

    checking reson of error by ownTest

--- subject_file/ownTest/bonus.sh
+++ subject_file/ownTest/bonus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc get_next_line_test.c ../GNL_lover/copy_in_here_GNL_files/get*_bonus.? -D BUFFER_SIZE=$1
+	gcc bonus_test.c ../get*_bonus.? -D BUFFER_SIZE=$1 
--- subject_file/ownTest/bonus_test.c
+++ subject_file/ownTest/bonus_test.c
@@ -15,29 +15,18 @@ void 	putstr(char *s, int fd)
 	}
 }
 
-void	main()
+int		main()
 {
 	volatile int fd;
 		fd = open("./for_test.txt", O_RDWR);
 	char 	* r= NULL;
-	char buf[100];
+	//char buf[100];
 	int i = SUCCESS;
 
-	char k[1];
-	k[0] = 0;
-	write(fd,"\n",1);
-	write(fd,k,1);
 //	while ((i = read(fd, buf, 4)));
 //	for (int i = 0; i < 4; i++)
 //		printf("i:%d|%d\n", i, (int)buf[i]);
-//
-//
-//
-//	while((i = get_next_line(fd, &r))== SUCCESS)
-//		printf("return :%d		len:%d		|%s",i,strlen(r),r);
-//		
-//	char *buff[10];
-//	int n = read(fd,buff[3],10);
-//	printf("%d:%s \n", n, buff[3]);
-//
+
+	while((i = get_next_line(fd, &r)) == SUCCESS)
+		printf("return :%d		len:%ld		|%s\n",i,strlen(r),r);
 }
