b0de970879ddd6986077ac7b77a721f323363bc1
Soengyong Kim <soekim@c1r7s4.42seoul.kr>
2021-01-27 18:03:37 +0900

    mktxt.c recoverd

---
+++ subject_file/ownTest/mktxt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <stdlib.h>
+
+
+
+void	maketxt(int fd, int len, int line)
+{
+	int temp = len;
+
+	while (--line >= 0)
+	{
+		len = temp;
+		char n = '1';
+
+		while (--len >= 0)
+
+		{
+			write(fd, &n , 1);
+			n++;
+		}
+		write(fd, "\n", 1);
+	}
+}
+
+int		main(int ac, char *av[])
+{
+	close(0);
+	int fd =  open("./for_test.txt", O_CREAT | O_RDWR | O_TRUNC);
+	if(ac)
+		maketxt(fd, atoi(av[1]), atoi(av[2]));
+	close(fd);
+}
