cd5583d2b5bda6270d5d01c33abd595634a1a44e
yong-dev <tjddyd1565@naver.com>
2021-01-29 16:19:10 +0900

    Declaration of both headers were wrong, but passed moulinette

--- subject_file/get_next_line_bonus.h
+++ subject_file/get_next_line_bonus.h
@@ -14,22 +14,17 @@
 # define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
-# include <stdio.h>
 # include <limits.h>
 # include <unistd.h>
-# include <sys/types.h>
-# include <sys/stat.h>
 
 # define ERROR		-1
 # define END		0
 # define SUCCESS	1
 
-size_t	ft_strlen(const char *s);
-char	*ft_strcpy(char *dst, const char *src);
+char		*ft_strncpy(char *dst, const char *src, int len);
 int		strdel_len(char *s, char delimiter);
 int		strcat_del(char **line, char *to_catenate, char delimiter);
-int		min(int a, int b);
 int		get_next_line(int fd, char **line);
-void	*ft_memmove(void *dst, const void *src, int len);
+void		*ft_memmove(void *dst, const void *src, int len);
 int		get_oneline_and_next(char **next, char *buffer, char **temp, int fd);
 #endif
