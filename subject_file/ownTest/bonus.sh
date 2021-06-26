f5d166daa4e61e2827285ff0a909afc6003d2d2a
yong-dev <tjddyd1565@naver.com>
2021-06-27 01:19:29 +0900

    done

--- subject_file/ownTest/bonus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm ./a.out
-	gcc bonus_test.c ../get*_bonus.? -D BUFFER_SIZE=$1 
-./a.out | cat -e
-rm ./a.out
--- subject_file/ownTest/comp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc get_next_line_test.c ../GNL_lover/copy_in_here_GNL_files/get_next_line.c  ../GNL_lover/copy_in_here_GNL_files/get_next_line_utils.c -D BUFFER_SIZE=$1
